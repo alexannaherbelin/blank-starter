4932f38cadd8fedae7035f494f350835f167a168
alexannaherbelin <alexannaherbelin@students.berkeley.net>
2025-08-20 17:49:35 +0000

    Finished

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("savebutton").addEventListener("click",save);
-ducument.getElementById("lastbutton").addEventListener("click",lastEntry);
+document.getElementById("returnbutton").addEventListener("click",retrieve);
 const returned = document.getElementById("returned")
 //object
 let person = {name:""}
@@ -7,14 +7,16 @@ let person = {name:""}
 //saving function
 function save(){
     person.name = document.getElementById("personname").value
-    console.log(person.name);
-    stringname = JSON.stringify(person.name)
+    
+    console.log("Before saving: ", person.name);
+    stringname = JSON.stringify(person.name);
     localStorage.setItem("names", stringname);
 
 }
-function lastEntry(){
-    retrieved = localStorage.getItem("names");
-   returned.innerHTML = retrieved 
+function retrieve(){
+    retrieved = JSON.parse(localStorage.getItem("names"));
+    
+   returned.innerHTML = retrieved;
+   console.log("After retrieving: ", retrieved);
+
 }
-
-
