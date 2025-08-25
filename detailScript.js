e79a1d580c1dd3859e4794f9c499572130b6740a
alexannaherbelin <alexannaherbelin@students.berkeley.net>
2025-08-25 19:28:06 +0000

    editing starts

--- detailScript.js
+++ detailScript.js
@@ -1,4 +1,11 @@
+document.getElementById("saveeditbutton").addEventListener("click",saveEdits);
+document.getElementById("canceleditbutton").addEventListener("click",cancelEdits);
+
 const queryString = window.location.search;
+
+const arrayOfTypes = ["Model:", "Gears:", "Components:", "Tire Width:", "Weight:"]
+
+const edits = document.getElementById("edits")
 
 // Parse it using URLSearchParams
 const parameters = new URLSearchParams(queryString);
@@ -16,4 +23,17 @@ for(let i = 0; i<data.length; i++){
          + "<br/>Weight: " + data[id].weight;
  
     }
+}
+
+function saveEdits(){
+
+    for(let c = 0; c < arrayOfTypes.length; c++){
+        if(edits.include(arrayOfTypes[c])){
+            
+        }
+    }
+
+}
+function cancelEdits(){
+
 }
